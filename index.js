ab91cb96601b17efefeb783064bd744674cf1a09
plush-owl <36096859+plush-owl@users.noreply.github.com>
2020-03-28 13:44:06 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
-console.log('Hello Earth!');
+var str
+pronoun = document.getElementById('gender');
 
-var pronoun = document.getElementById('gender').value
+//var selectedPronoun = pronoun.options[pronoun.selectedIndex].value;
+//console.log(selectedPronoun);
 
-if (pronoun = 'He') {
+if (pronoun = 'Man') {
     console.log('It\'s a boy!');
-} else if (pronoun = 'She'){
+} else if (pronoun = 'Woman'){
     console.log('It\'s a girl!');
-} else if (pronoun = 'They'){
-    console.log('It\'s a person!')
-}
+} else if (pronoun = 'Person'){
+    console.log('It\'s a person!');
+}; // theory: if you select a diff gender and click fortune it should change the fortune
+
+function fortune(){
+    console.log('Insert Fortune!');
+};
+
+console.log(pronoun)
+
+document.getElementById('fortuneButton').onclick = function(){
+    //alert('button was clicked!');
+    console.log(pronoun)
+    if (pronoun == 'Woman'){
+        console.log('Sup bitch!');
+    } else if (pronoun == 'Person'){
+        console.log('Sup homosapien!');
+    } else if (pronoun == 'Man'){
+        console.log('Sup Bruh!');
+    } else {
+        console.log('Who are you?!');
+    } 
+
+    /*switch (pronoun){
+        case 'He':
+            console.log('Hey bro');
+            break;
+        case 'She':
+            console.log('Hey sis');
+            break;
+        case 'They':
+            console.log('Hey person')
+            break;
+    }*/
+}; 
+
+console.log(`Hello there, ${pronoun}.`)
