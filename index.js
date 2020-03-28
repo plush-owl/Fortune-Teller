d6c3fbea65bd9cda7360d183a6f72bd2a1de379f
plush-owl <36096859+plush-owl@users.noreply.github.com>
2020-03-28 11:37:15 -0700

    I added stuff

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log('Hello Earth!');
+
+if (document.getElementById('gender') = 'He') {
+    console.log('It\'s a boy!');
+} else if (document.getElementById('gender') = 'She'){
+    console.log('It\'s a girl!');
+} else if (document.getElementById('gender') = 'They'){
+    console.log('It\'s a person!')
+}
