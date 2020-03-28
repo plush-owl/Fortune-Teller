c733940a737863a19cdc5b713eb2463933b3d4c8
plush-owl <36096859+plush-owl@users.noreply.github.com>
2020-03-28 11:39:46 -0700

    added some values

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 console.log('Hello Earth!');
 
-if (document.getElementById('gender') = 'He') {
+if (document.getElementById('gender').value = 'He') {
     console.log('It\'s a boy!');
-} else if (document.getElementById('gender') = 'She'){
+} else if (document.getElementById('gender').value = 'She'){
     console.log('It\'s a girl!');
-} else if (document.getElementById('gender') = 'They'){
+} else if (document.getElementById('gender').value = 'They'){
     console.log('It\'s a person!')
 }
