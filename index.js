8027ffe1178365203203ce92405ee192739f69bb
RamieShreim <44176786+RamieShreim@users.noreply.github.com>
2020-03-28 15:11:36 -0700

    updated js and added jquery

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 //var str
 pronoun = document.getElementById('gender');
 
+//resulting fortune after random number
+var outputFortune;
+
 var selectedPronoun = pronoun.options[pronoun.selectedIndex].value;
 //console.log(selectedPronoun);
+
+//clears text for new fortune
+function text_clear() { 
+        
+    //Clears game for next click
+    $("#output").html("")
+}
 
 function fortune(){
     console.log('Insert Fortune!');
@@ -20,20 +30,26 @@ document.getElementById('fortuneButton').onclick = function(){
 
     switch(fortuneNumber){
         case 0:
-            console.log(`Hey, ${selectedPronoun}! Your number is ${fortuneNumber}.`);
+            outputFortune = (`Hey, ${selectedPronoun}! Your number is ${fortuneNumber}.`);
             break;
         case 1:
-            console.log(`Hey, ${selectedPronoun}! Your number is ${fortuneNumber}.`);
+            outputFortune = (`Hey, ${selectedPronoun}! Your number is ${fortuneNumber}.`);
             break;
         case 2:
-            console.log(`Hey, ${selectedPronoun}! Your number is ${fortuneNumber}.`);
+            outputFortune = (`Hey, ${selectedPronoun}! Your number is ${fortuneNumber}.`);
             break;
         case 3:
-            console.log(`Hey, ${selectedPronoun}! Your number is ${fortuneNumber}.`);
+            outputFortune = (`Hey, ${selectedPronoun}! Your number is ${fortuneNumber}.`);
             break;
         case 4:
-            console.log(`Hey, ${selectedPronoun}! Your number is ${fortuneNumber}.`);
+            outputFortune = (`Hey, ${selectedPronoun}! Your number is ${fortuneNumber}.`);
             break;
     }
+
+    //clears the output area so that fortunes do not stack.
+    text_clear();
+
+    //appends the fortune to the html so that you can see it on the page.
+    $( "#output" ).append(outputFortune); 
 
 }; 
