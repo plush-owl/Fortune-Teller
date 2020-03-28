39005df85b47fed8341f2ff02331a2f1109a4017
plush-owl <36096859+plush-owl@users.noreply.github.com>
2020-03-28 14:19:31 -0700

    merged switch with click code

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-pronoun = document.getElementById('gender');
+//var str
+pronoun = document.getElementById('gender').value;
 
 //var selectedPronoun = pronoun.options[pronoun.selectedIndex].value;
 //console.log(selectedPronoun);
@@ -12,7 +13,7 @@ function fortune(){
 document.getElementById('fortuneButton').onclick = function(){
     //alert('button was clicked!');
     console.log(pronoun)
-    /* if (pronoun == 'Woman'){
+    if (pronoun == 'Woman'){
         console.log('Sup bitch!');
     } else if (pronoun == 'Person'){
         console.log('Sup homosapien!');
@@ -20,26 +21,26 @@ document.getElementById('fortuneButton').onclick = function(){
         console.log('Sup Bruh!');
     } else {
         console.log('Who are you?!');
-    } */  //For Testing
+    }   //For Testing
+
+    let fortuneNumber = Math.floor(Math.random() * 5); //will change num later
+
+    switch(fortuneNumber){
+        case 0:
+            console.log(`Hey, ${pronoun}! Your number is ${fortuneNumber}.`);
+            break;
+        case 1:
+            console.log(`Hey, ${pronoun}! Your number is ${fortuneNumber}.`);
+            break;
+        case 2:
+            console.log(`Hey, ${pronoun}! Your number is ${fortuneNumber}.`);
+            break;
+        case 3:
+            console.log(`Hey, ${pronoun}! Your number is ${fortuneNumber}.`);
+            break;
+        case 4:
+            console.log(`Hey, ${pronoun}! Your number is ${fortuneNumber}.`);
+            break;
+    }
 
 }; 
-
-let fortuneNumber = Math.floor(Math.random() * 5); //will change num later
-
-switch(fortuneNumber){
-    case 0:
-        console.log(`Hey, ${pronoun}! Your number is ${fortuneNumber}.`);
-        break;
-    case 1:
-        console.log(`Hey, ${pronoun}! Your number is ${fortuneNumber}.`);
-        break;
-    case 2:
-        console.log(`Hey, ${pronoun}! Your number is ${fortuneNumber}.`);
-        break;
-    case 3:
-        console.log(`Hey, ${pronoun}! Your number is ${fortuneNumber}.`);
-        break;
-    case 4:
-        console.log(`Hey, ${pronoun}! Your number is ${fortuneNumber}.`);
-        break;
-}
